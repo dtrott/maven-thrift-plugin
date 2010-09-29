3a9c1c9b9a6c8e94c5aa3580b1aa23b4b87658b6
David Trott <github@davidtrott.com>
2010-09-28 19:05:08 -0700

    Changed default options to include hashcode generation.

--- src/main/java/org/apache/thrift/maven/AbstractThriftMojo.java
+++ src/main/java/org/apache/thrift/maven/AbstractThriftMojo.java
@@ -80,7 +80,7 @@ abstract class AbstractThriftMojo extends AbstractMojo {
      * it will generate Java output. The main reason for this option is to be able to add options
      * to the Java generator - if you generate something else, you're on your own.
      *
-     * @parameter default-value="java"
+     * @parameter default-value="java:hashcode"
      */
     private String generator;
 
